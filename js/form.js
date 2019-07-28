c6c82ab873673524ea952329a1ff43cb848a1384
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-07-28 22:07:09 +0300

    код задания 8/3 рабочий

--- js/form.js
+++ js/form.js
@@ -2,13 +2,20 @@
 
 (function () {
 
+  var MIN_PRICE_HOUSING = [0, 1000, 5000, 10000];
+  var MAX_ROOM = '100';
+  var NOT_CAPACITY = '0';
+
   var mapFilter = document.querySelector('.map__filters'); // нахожу блок формы фильтров
   var adForm = document.querySelector('.ad-form'); // нахожу блок формы объявления
   var priceInput = adForm.querySelector('#price'); // нахожу поле с ценой
   var adFormSelectType = adForm.querySelector('#type'); // нахожу select с типом жилья
+  var adFormOptionsType = Array.from(adFormSelectType.querySelectorAll('option')); // option с типом жилья
   var adFormFieldsetTime = adForm.querySelector('.ad-form__element--time'); // нахожу fieldset time
-  var adFormSelectTimeIn = adForm.querySelector('#timein'); // нахожу select timein
-  var adFormSelectTimeOut = adForm.querySelector('#timeout'); // нахожу select timeout
+  var adFormSelectTimeIn = adFormFieldsetTime.querySelector('#timein'); // нахожу select timein
+  var adFormSelectTimeOut = adFormFieldsetTime.querySelector('#timeout'); // нахожу select timeout
+  var adFormSelectRoomNumber = adForm.querySelector('#room_number');
+  var adFormSelectCapacity = adForm.querySelector('#capacity');
 
   var addAttributs = function (collection) { // функция пребразования HTML коллекции в массив и добавления атрибутов
     Array.from(collection).forEach(function (it) {
@@ -33,16 +40,18 @@
   window.form.mapFilterFieldsetElem.setAttribute('disabled', ''); // блокируем fieldset в форме фильтров
 
   // Код фильтрации в форме объявления
-  var minPriceHousing = {
-    bungalo: 0,
-    flat: 1000,
-    house: 5000,
-    palace: 10000
-  };
+  // код для полей тип жилья и цена
+  var adFormOptionsTypeValue = adFormOptionsType.map(function (it) { // получение value option с типом жилья
+    return it.value;
+  });
 
-  var onTypeChange = function () { // создаю ф-ю обработчика события для изменения типа и цены жилья
+  // создал словарь тип жилья и цена
+  var typeHousingToPrice = window.utils.getArrsToObject(adFormOptionsTypeValue, MIN_PRICE_HOUSING);
+
+  // создаю ф-ю обработчика события для изменения типа и цены жилья
+  var onTypeChange = function () {
     var value = adFormSelectType.value; // записал в пер-ю номер выбранного элемента option
-    var newPrice = minPriceHousing[value];
+    var newPrice = typeHousingToPrice[value];
 
     priceInput.placeholder = newPrice; // присваиваю значение placeholder
     priceInput.min = newPrice; // присваиваю значение min для поля с ценой
@@ -50,11 +59,37 @@
 
   adFormSelectType.addEventListener('change', onTypeChange); // навешиваю обработчик с переданной ф-й на select с типом жилья
 
-  var onTimeChange = function (evt) { // создаю ф-ю обработчика события для изменения заезда и выезда
+  // код для полей время заезда и выезда
+  // создаю ф-ю обработчика события для изменения заезда и выезда
+  var onTimeChange = function (evt) {
     adFormSelectTimeIn.value = evt.target.value;
     adFormSelectTimeOut.value = evt.target.value;
   };
 
   adFormFieldsetTime.addEventListener('change', onTimeChange); // навешиваю обработчик с переданной ф-й на fieldset c двумя select времени заезда и выезда
 
+  // код для полей кол-во комнат и кол-во мест
+
+  var onRoomChange = function () {
+    var valueRoom = adFormSelectRoomNumber.value;
+    var valueCapacity = adFormSelectCapacity.value;
+
+    if (valueRoom < valueCapacity) {
+      adFormSelectCapacity.setCustomValidity('Значение должно быть, меньше или равно количеству комнат');
+    } else if (valueRoom === MAX_ROOM && !(valueCapacity === NOT_CAPACITY)) {
+      adFormSelectCapacity.setCustomValidity('Значение должно быть равно, "не для гостей"');
+    } else {
+      adFormSelectCapacity.setCustomValidity('');
+    }
+  };
+
+  adFormSelectRoomNumber.addEventListener('change', onRoomChange);
+  adFormSelectCapacity.addEventListener('change', onRoomChange);
+
 })();
+
+// [{
+//   fieldName: 'address',
+//   fieldValue: '',
+//   errorMessage: 'is required'
+// }]
--- js/utils.js
+++ js/utils.js
@@ -12,10 +12,17 @@
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
 
-    isEscEvent: function (evt, action) {
+    isEscEvent: function (evt, action) { // ф-я проверки нажатия кнопки esc и действия после нажатия
       if (evt.keyCode === ESC_KEYCODE) {
         action();
       }
+    },
+
+    getArrsToObject: function (keys, vals) { // ф-я создания объекта словаря из двух массивов
+      return keys.reduce(function (prev, val, i) {
+        prev[val] = vals[i];
+        return prev;
+      }, {});
     }
   };
 
