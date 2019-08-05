8ac659885363b8714dc35d546db69ee3a16aab5b
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-08-05 18:02:56 +0300

    код фильтров++

--- js/filter.js
+++ js/filter.js
@@ -7,7 +7,6 @@
   var similarMapPin = document.querySelector('.map__pins'); // находим блок для вставки меток
   var mapFilter = document.querySelector('.map__filters-container'); // блок фильтрации на карте
   var formFilter = mapFilter.querySelector('.map__filters'); // блок формы фильтрации
-  var selectHousingType = formFilter.querySelector('#housing-type'); // селект с типами жилья
   var selectHousingPrice = formFilter.querySelector('#housing-price'); // селект с ценой жилья
   var optionsHousingPrice = Array.from(selectHousingPrice.querySelectorAll('option'));
 
@@ -48,21 +47,60 @@
     features: []
   };
 
+  var priceState = getPriceHousing(); // получил словарь с значениями цен
+  var HIGH_PRICE = parseInt(priceState.middle.split('-')[1], 10); // max цена
+  var LOW_PRICE = parseInt(priceState.middle.split('-')[0], 10); // min цена
+
   var onFilterChange = function (evt) {
-    // debugger;
+    var checked = evt.target.checked;
+
     if (evt.target.tagName === 'SELECT') {
       filterState[evt.target.id.split('-')[1]] = evt.target.value;
-    } else {
+    } if (checked) {
       filterState.features.push(evt.target.defaultValue);
+    } else if (!checked) {
+      filterState.features.pop(evt.target.defaultValue);
     }
 
-    var filteredPins = window.pin.data.filter(function (pin) {
-      if (pin.offer.type !== filterState.type || filterState.type !== 'any') {
+    // console.log(filterState);
+
+    var filteredPins = window.pin.data.filter(function (pin) { // фильтрую метки
+      if (filterState.type !== 'any') {
+        if (pin.offer.type !== filterState.type) {
+          return false;
+        }
+      }
+
+      if (filterState.price !== 'any') {
+        if (filterState.price === 'middle') {
+          return pin.offer.price > LOW_PRICE && pin.offer.price < HIGH_PRICE;
+        } else if (filterState.price === 'low') {
+          return pin.offer.price < LOW_PRICE;
+        } else if (filterState.price === 'high') {
+          return pin.offer.price > HIGH_PRICE;
+        }
         return false;
       }
-      if (pin.offer.rooms !== +filterState.rooms) {
+
+      if (filterState.rooms !== 'any') {
+        if (pin.offer.rooms !== +filterState.rooms) {
+          return false;
+        }
+      }
+
+      if (filterState.guests !== 'any') {
+        if (pin.offer.guests !== +filterState.guests) {
+          return false;
+        }
+      }
+
+      if (filterState.features !== []) {
+        filterState.features.find(function (elem) {
+          return pin.offer.features[elem];
+        });
         return false;
       }
+
       return true;
     });
 
