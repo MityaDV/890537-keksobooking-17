0888c658f24c9b59dcb3c350e38b5c6486b1ea21
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-08-05 12:50:54 +0300

    код фильтров

--- js/filter.js
+++ js/filter.js
@@ -3,18 +3,13 @@
 (function () {
   var MAX_NUMBER_PIN = 5;
 
+  var map = document.querySelector('.map'); // нахожу блок карты
   var similarMapPin = document.querySelector('.map__pins'); // находим блок для вставки меток
   var mapFilter = document.querySelector('.map__filters-container'); // блок фильтрации на карте
   var formFilter = mapFilter.querySelector('.map__filters'); // блок формы фильтрации
   var selectHousingType = formFilter.querySelector('#housing-type'); // селект с типами жилья
   var selectHousingPrice = formFilter.querySelector('#housing-price'); // селект с ценой жилья
   var optionsHousingPrice = Array.from(selectHousingPrice.querySelectorAll('option'));
-  // var selectHousingRoom = formFilter.querySelector('#housing-rooms'); // селект с числом комнат
-  // var selectHousingGuest = formFilter.querySelector('#housing-guests'); // селект с числом гостей
-  // var selectHousingFeature = formFilter.querySelector('#housing-features'); // фиелдсет с удоюствами
-  // var inputMapFeatures = selectHousingFeature.querySelectorAll('.map__feature'); // фиелдсет с удоюствами
-
-  var map = document.querySelector('.map'); // нахожу блок карты
 
   // удаляю метки и открытые карточки
   var getClearMap = function () {
@@ -45,6 +40,36 @@
     return valuePriceToNumberPrice;
   };
 
+  var filterState = {
+    type: 'any',
+    price: 'any',
+    rooms: 'any',
+    guests: 'any',
+    features: []
+  };
+
+  var onFilterChange = function (evt) {
+    // debugger;
+    if (evt.target.tagName === 'SELECT') {
+      filterState[evt.target.id.split('-')[1]] = evt.target.value;
+    } else {
+      filterState.features.push(evt.target.defaultValue);
+    }
+
+    var filteredPins = window.pin.data.filter(function (pin) {
+      if (pin.offer.type !== filterState.type) {
+        return false;
+      }
+      if (pin.offer.rooms !== +filterState.rooms) {
+        return false;
+      }
+      return true;
+    });
+
+    getClearMap();
+    window.filter.getRenderPin(filteredPins);
+  };
+
   window.filter = {
 
     getRenderPin: function (data) { // ф-я создания пяти меток
@@ -58,52 +83,8 @@
       similarMapPin.appendChild(fragment); // вставляем сформированный фрагмент в разметку
     },
 
-    getChangeHousingFilter: function (data) { // ф-я фильтра типа жилья
-
-      var onHousingChangeType = function (evt) { // выбраное значение типа жилья
-        getClearMap();
-
-        var newValue = evt.target.value;
-
-        var newPinType = data.filter(function (elem) { // фильтрую массив по типу жилья
-          return elem.offer.type === newValue;
-        });
-
-        var fragment = document.createDocumentFragment(); // создаём елемент fragment
-
-        newPinType.slice(0, MAX_NUMBER_PIN).forEach(function (elem) { // перебираю отфильтрованный массив
-          fragment.appendChild(window.pin.render(elem));
-        });
-
-        similarMapPin.appendChild(fragment);
-      };
-
-      var onHousingChangePrice = function (evt) {
-        getClearMap();
-
-        var newValue = evt.target.value;
-
-        var price = getPriceHousing();
-        var regex = /[\d|,|.|e|E|\+]+/g;
-        var priceItem = price[newValue].match(regex);
-
-        var newPinPrice = data.filter(function (elem) { // фильтрую массив по цене жилья
-          return elem.offer.price < priceItem;
-        });
-        // return newPinPrice;
-        var fragment = document.createDocumentFragment(); // создаём елемент fragment
-
-        newPinPrice.slice(0, MAX_NUMBER_PIN).forEach(function (elem) { // перебираю отфильтрованный массив
-          fragment.appendChild(window.pin.render(elem));
-        });
-
-        similarMapPin.appendChild(fragment);
-      };
-
-      selectHousingType.addEventListener('change', onHousingChangeType); // обработчик смены типа жилья
-      selectHousingPrice.addEventListener('change', onHousingChangePrice); // обработчик смены цены жилья
-      // selectHousingRoom.addEventListener('change', onHousingChangeFilter); // обработчик смены кол-ва комнат
-      // selectHousingGuest.addEventListener('change', onHousingChangeFilter); // обработчик смены кол-ва жильцов
+    getChangeHousingFilter: function () {
+      formFilter.addEventListener('change', onFilterChange);
     }
   };
 
